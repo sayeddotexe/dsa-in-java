2bed554b218409a844078a293f8900a4b2d28749
Sayed Ismail Ahmed <contact.ismailahmed@gmail.com>
2025-07-18 08:44:45 +0100

    feat: added java built implementation for singly and doubly linked list

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import arrays.ArrayBasics;
 import arrays.DoublyLinkedList;
 import arrays.SinglyLinkedList;
 
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 public class Main {
     public static void main(String[] args) {
         ArrayBasics.demo();
@@ -62,6 +65,28 @@ public class Main {
          */
 
         System.out.println(" ");
+        System.out.println("Java Built-in Linked List (and doubly)");
+
+        // Use generics for type safety
+        LinkedList<Integer> javaLinkedList = new LinkedList<>();
+
+        javaLinkedList.add(2);
+        javaLinkedList.add(4);
+        javaLinkedList.add(6);
+        javaLinkedList.add(8);
+
+        System.out.println(javaLinkedList);  // Output: [2, 4, 6, 8]
+
+        //ListIterator is an interface in Java that allows you to traverse a list in both directions —
+        // forward and backward — and also modify the list while iterating.
+
+        System.out.println("\nBackward traversal:");
+        ListIterator<Integer> itr = javaLinkedList.listIterator(javaLinkedList.size());
+        while (itr.hasPrevious()) {
+            System.out.print(itr.previous() + " ");
+        }
+
+
     }
 
 }
